379b8496b9317398d1ae69b37d64fab4b152b6f0
zhuichao001 <zhuichao001@163.com>
2022-03-07 19:43:36 +0800

    add inline for util

--- util.h
+++ util.h
@@ -3,15 +3,15 @@
 
 #include <string>
 
-bool startswith(const std::string &s, const std::string &sub){
+inline bool startswith(const std::string &s, const std::string &sub){
     return s.find(sub)==0 ? true : false;
 }
 
-bool endswith(const std::string &s, const std::string &sub){
+inline bool endswith(const std::string &s, const std::string &sub){
     return s.rfind(sub)==(s.length()-sub.length()) ? true : false;
 }
 
-const char* filename(const char *path) {
+inline const char* filename(const char *path) {
     const char *p = strrchr(path,'/');
     return p ? p+1 : path;
 }
